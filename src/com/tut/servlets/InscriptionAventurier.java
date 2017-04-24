4d34de81148122480c222f14d1dfa817061acec1
Eric Dasse <Eric Dasse>
2017-04-24 17:41:13 +0200

    feat: initialisation du DAOFactory dans chaque fichier

--- src/com/tut/servlets/InscriptionAventurier.java
+++ src/com/tut/servlets/InscriptionAventurier.java
@@ -1,6 +1,7 @@
 package com.tut.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.tut.beans.Utilisateur;
+import com.tut.dao.DAOFactory;
+import com.tut.dao.UtilisateurDAO;
 import com.tut.forms.FormInscription;
 
 /**
@@ -16,9 +19,20 @@ import com.tut.forms.FormInscription;
 @WebServlet("/inscription_aventurier")
 public class InscriptionAventurier extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	public static final String CONF_DAO_FACTORY = "daofactory";
 	public static final String VUE = "/WEB-INF/jsp/Inscription_aventurier.jsp";
 	public static final String ATT_USER = "utilisateur";
 	public static final String ATT_FORM = "form";
+	public static final String ATT_USER_TYPE = "type";
+	
+	private UtilisateurDAO utilisateurDao;
+	
+	public void init() {
+		this.utilisateurDao = ( (DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY) ).
+				getUtilisateurDao();
+	}
+	
+	
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -38,8 +52,8 @@ public class InscriptionAventurier extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		FormInscription form = new FormInscription();
-		
+		FormInscription form = new FormInscription(utilisateurDao);
+		request.setAttribute(ATT_USER_TYPE, new String("Aventuriers"));
 		Utilisateur utilisateur = form.inscrireUtilisateur(request);
 		
 		request.setAttribute(ATT_USER, utilisateur);
