dc4a38b6c7f50425c086de6e3ee02eafaa365405
Eric Dasse <Eric Dasse>
2017-04-29 21:27:28 +0200

    add: AventurierDAO.java

---
+++ src/com/tut/dao/AventurierDAO.java
@@ -0,0 +1,19 @@
+package com.tut.dao;
+
+import com.tut.beans.Utilisateur;
+
+public class AventurierDAO implements UtilisateurDAO {
+
+	@Override
+	public void creer(Utilisateur utilisateur) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Utilisateur trouver(String email) throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
