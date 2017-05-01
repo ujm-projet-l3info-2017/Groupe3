5eb2135e6cc2a525587b3f187bff0bad10369e6c
Eric Dasse <Eric Dasse>
2017-05-01 14:53:47 +0200

    modify: Connexion.java: initialiasation du DAO utilisateur

--- src/com/tut/servlets/Connexion.java
+++ src/com/tut/servlets/Connexion.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.tut.beans.Utilisateur;
+import com.tut.dao.DAOFactory;
+import com.tut.dao.UtilisateurDAO;
 import com.tut.forms.ConnexionForm;
 
 /**
@@ -19,10 +21,18 @@ import com.tut.forms.ConnexionForm;
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String VUE = "/WEB-INF/jsp/connexion.jsp";
+	public static final String CONF_DAO_FACTORY = "daofactory";
 	public static final String ATT_USER_SESSION = "sessionUtilisateur";
 	public static final String ATT_USER = "utilisateur";
 	public static final String ATT_FORM = "form";
 	public static final String URL_REDIRECTION = "home";
+	
+	private UtilisateurDAO utilisateurDao;
+	
+	public void init() {
+		this.utilisateurDao = ( (DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY) ).
+				getUtilisateurDao();
+	}
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -42,7 +52,7 @@ public class Connexion extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ConnexionForm form = new ConnexionForm();
+		ConnexionForm form = new ConnexionForm(utilisateurDao);
 		
 		Utilisateur utilisateur = form.connecterUtilisateur(request);
 		
