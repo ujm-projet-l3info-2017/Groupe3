6fda30cb8a64777dd6c438551f12a936c201096b
Eric Dasse <Eric Dasse>
2017-05-10 00:33:31 +0200

    modify: AventurierDAO.java: corrections sur la requête, récupération des données aventuriers

--- src/com/tut/dao/AventurierDAO.java
+++ src/com/tut/dao/AventurierDAO.java
@@ -1,18 +1,19 @@
 package com.tut.dao;
 
+import static com.tut.dao.DAOUtilitaire.fermeturesSilencieuses;
+import static com.tut.dao.DAOUtilitaire.initialisationRequetePreparee;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import com.tut.beans.Aventurier;
-
-import static com.tut.dao.DAOUtilitaire.fermeturesSilencieuses;
-import static com.tut.dao.DAOUtilitaire.initialisationRequetePreparee;
+import com.tut.beans.Utilisateur;
 
 public class AventurierDAO extends UtilisateurDaoImpl {
-	private static final String SQL_SELECT_PAR_ID = "SELECT idAventurier, lvl, expTotale, "
-			+ "gold, rubis, saphir, classe WHERE idAventurier = ?";
+	private static final String SQL_SELECT_PAR_ID = "SELECT lvl, expTotale, "
+			+ "gold, rubis, saphir, classe FROM aventuriers WHERE idAventurier = ?";
 	
 	public AventurierDAO(DAOFactory daoFactory) {
 		super (daoFactory );
@@ -29,12 +30,13 @@ public class AventurierDAO extends UtilisateurDaoImpl {
 		Connection connexion = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
-		Aventurier aventurier = (Aventurier) super.trouver(email);
+		Utilisateur user = super.trouver(email);
+		Aventurier aventurier = new Aventurier();
 		
 		
 		try {
 			connexion = daoFactory.getConnection();
-			preparedStatement = initialisationRequetePreparee(connexion, SQL_SELECT_PAR_ID, true, aventurier.getId());
+			preparedStatement = initialisationRequetePreparee(connexion, SQL_SELECT_PAR_ID, true, user.getId());
 			resultSet = preparedStatement.executeQuery();
 			
 			if (resultSet.next()) {
